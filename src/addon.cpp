1f2efa756f2510a6936187ff9c96e90339af9478
Alwin Esch <alwin.esch@web.de>
2016-12-29 09:33:00 +0100

    Update addon instance handle functions

--- src/addon.cpp
+++ src/addon.cpp
@@ -206,11 +206,11 @@ extern "C" void ADDON_Announce(const char *flag, const char *sender, const char 
 {
 }
 
-extern "C" ADDON_STATUS ADDON_CreateInstance(int instanceType, const char* instanceID, const void* instanceProps, void* instanceFunctions, void* kodiInstance, void** addonInstance)
+extern "C" ADDON_STATUS ADDON_CreateInstance(int instanceType, const char* instanceID, KODI_HANDLE instance, KODI_HANDLE* addonInstance)
 {
   return ADDON_STATUS_UNKNOWN;
 }
 
-extern "C" void ADDON_DestroyInstance(int instanceType, const char* instanceID, void* instance)
+extern "C" void ADDON_DestroyInstance(int instanceType, KODI_HANDLE instance)
 {
 }
